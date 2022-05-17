a187be5fece19b01af02510810249529f146e85c
Romero Reis <romero_serra48@hotmail.com>
2022-05-17 16:56:21 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/praticaweb/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.praticaweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.praticaweb.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
