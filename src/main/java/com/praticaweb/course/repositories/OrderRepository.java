383d2739789dcfbae631f916bf841d29a54b66bb
Romero Reis <romero_serra48@hotmail.com>
2022-05-20 11:27:09 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/praticaweb/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.praticaweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.praticaweb.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
